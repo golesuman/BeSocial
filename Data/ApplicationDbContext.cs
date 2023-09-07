8b630fe02f5cf37921a7a99935fd9a4472b1b892
Suman Gole <gole35830@gmail.com>
2023-09-07 16:27:41 +0545

    feat: seed identity users and roles in db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using BlogApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,8 @@ namespace BlogApp.Data;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    private readonly RoleManager<IdentityRole> _roleManager;
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
@@ -14,4 +17,44 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Post> Posts { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder builder)  
+    {  
+        base.OnModelCreating(builder);  
+        // this.SeedUsers(builder);  
+        this.SeedRoles(builder);
+        // this.SeedUserRoles(builder);  
+    }  
+  
+    private void SeedUsers(ModelBuilder builder)  
+    {  
+        ApplicationUser user = new ()  
+        {  
+            Id = "b74ddd14-6340-4840-95c2-db12554843e5",  
+            UserName = "Admin",  
+            Email = "admin@gmail.com",  
+            LockoutEnabled = false,  
+            PhoneNumber = "1234567890"  
+        };  
+  
+        PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();  
+        user.PasswordHash = passwordHasher.HashPassword(user, "Admin*123");  
+  
+        builder.Entity<ApplicationUser>().HasData(user);  
+    }  
+  
+    private void SeedRoles(ModelBuilder builder)  
+    {  
+        builder.Entity<IdentityRole>().HasData(  
+            new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },  
+            new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", Name = "HR", ConcurrencyStamp = "2", NormalizedName = "Human Resource" }  
+        );  
+    }  
+  
+    private void SeedUserRoles(ModelBuilder builder)  
+    {  
+        builder.Entity<IdentityUserRole<string>>().HasData(  
+            new IdentityUserRole<string>() { RoleId = "fab4fac1-c546-41de-aebc-a14da6895711", UserId = "b74ddd14-6340-4840-95c2-db12554843e5" }  
+        );  
+    }  
 }
---
+++ Migrations/20230902115114_seed  roles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlogApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedroles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "c7b013f0-5201-4317-abd8-c211f91b7330", "2", "HR", "Human Resource" },
+                    { "fab4fac1-c546-41de-aebc-a14da6895711", "1", "Admin", "Admin" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7b013f0-5201-4317-abd8-c211f91b7330");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fab4fac1-c546-41de-aebc-a14da6895711");
+        }
+    }
+}
